d3d020c6fcfcae07c7eec5a4564c3ac208e43f9d
mouhib <mouhibouni321@gmail.com>
2021-07-11 09:54:08 +0100

    added checks for winner

--- src/com/mouhib/Main.java
+++ src/com/mouhib/Main.java
@@ -13,11 +13,10 @@ public class Main {
                 {'7', '8', '9'}
         };
 
-        char symbol = getSymbol();
-        char choice = choice(board);
-        boardPrinter(board);
-        filler(board,choice,symbol);
-        boardPrinter(board);
+//        char symbol = getSymbol();
+//        char choice = choice(board);
+//        filler(board,choice,symbol);
+//        boardPrinter(board);
 
     }
 
@@ -75,12 +74,44 @@ public class Main {
         }
     }
 
-    public static boolean GameOver(char [][] board){
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
 
-            }
-        }
-        return true;
+    /*
+            ----+---+----
+            | 1 | 2 | 3 |
+            ----+---+----
+            | 4 | 5 | 6 |
+            ----+---+----
+            | 7 | 8 | 9 |
+            ----+---+----
+    */
+
+
+    public static boolean Winner(char[][] board, char symbol){
+        //straight horizontal
+        if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] == symbol) {return true;}
+        if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] == symbol) {return true;}
+        if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] == symbol) {return true;}
+
+        //straight vertical
+        if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] == symbol) {return true;}
+        if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] == symbol) {return true;}
+        if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] == symbol) {return true;}
+
+        //diagonal top left to buttom right
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == symbol) {return true;}
+
+        //diagonal top right to buttom left
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == symbol) {return true;}
+
+        return false;
     }
+
+//    public static boolean Draw(char [][] board){
+//        for (int i = 0; i < board.length; i++) {
+//            for (int j = 0; j < board.length; j++) {
+//                if (board[i][j] ! in  )
+//            }
+//        }
+//        return true;
+//    }
 }
